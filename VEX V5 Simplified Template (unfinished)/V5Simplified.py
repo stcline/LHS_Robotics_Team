2765166104dc0283538c80dbcde2905beb55d079
Detonate1111 <143915344+Detonate1111@users.noreply.github.com>
2024-09-12 11:31:43 -0700

    V5Simplified.py

--- VEX V5 Simplified Template (unfinished)/V5Simplified.py
+++ VEX V5 Simplified Template (unfinished)/V5Simplified.py
@@ -1,1 +1,73 @@
 
+# ---------------------------------------------------------------------------- #
+# 	Module:  V5Simplified.py                                                   #
+#                                                                              #
+# 	Description:  VEX V5 Starter Module - Simplified VEX V5 Module to help     #
+#                 students get accustomed to programming VEX Components.       #
+# ---------------------------------------------------------------------------- #
+
+from vex import *
+
+brain = Brain()
+controller = Controller()
+
+def settings():
+    pass
+
+def help():
+    print("Commands")
+
+class Controller:
+
+    def dpad():
+
+        while True:
+
+            if controller.buttonDown.pressing():
+                return "Down"
+
+            elif controller.buttonUp.pressing():
+                return "Up"
+
+            elif controller.buttonLeft.pressing():
+                return "Left"
+
+            elif controller.buttonRight.pressing():
+                return "Right"
+
+            else:
+                return None
+    
+    def faceButtons():
+        while True:
+
+            if controller.buttonA.pressing():
+                return "A"
+
+            elif controller.buttonB.pressing():
+                return "B"
+
+            elif controller.buttonX.pressing():
+                return "X"
+
+            elif controller.buttonY.pressing()():
+                return "Y"
+
+            else:
+                return None
+
+    def Joystick():
+        pass
+
+    def L1_R1():
+        pass
+
+    def L2_R2():
+        pass
+
+class Motor:
+    def MotorSetup():
+        pass
+
+class Sensor:
+    pass
