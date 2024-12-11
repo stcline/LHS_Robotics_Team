8d024895ac243b44623dba36e485c3bd9054c447
alecas6726 <alecas6726@stu.mylusd.org>
2024-12-11 09:42:39 -0800

    Update starter.cpp

--- code/starter.cpp
+++ code/starter.cpp
@@ -5,8 +5,8 @@ using namespace vex;
 // Create a Brain object
 vex::brain Brain;
 
-//vex::motor motor1(vex::PORT1); // Initialize motor1 with the correct port number
-vex::motor motor2(vex::PORT2); // Initialize motor2 with the correct port number
+//vex::motor motor11(vex::PORT11); // Initialize motor1 with the correct port number
+vex::motor motor20(vex::PORT20); // Initialize motor2 with the correct port number
 vex::controller Controller1; // Initialize Controller1
 // attach a 3 wire limit switch to the Bumper port
 vex::limit LimitSwitch = vex::limit(Brain.ThreeWirePort.B);
@@ -20,15 +20,15 @@ void pre_auton() {
 }
 
 void autonomous() {
-    // motor2.spin(vex::directionType::fwd, 50, vex::velocityUnits::pct);
+    // motor20.spin(vex::directionType::fwd, 50, vex::velocityUnits::pct);
     // Brain.Screen.print("Motor spinning forward");
     // vex::task::sleep(2000);
-    // motor2.stop();
+    // motor20.stop();
     // vex::task::sleep(2000);
-    // motor2.spin(vex::directionType::rev, 50, vex::velocityUnits::pct);
+    // motor20.spin(vex::directionType::rev, 50, vex::velocityUnits::pct);
     // Brain.Screen.print("Motor spinning reverse");
     // vex::task::sleep(2000);
-    // motor2.stop();
+    // motor20.stop();
     // vex::task::sleep(2000);
 }
 
@@ -36,22 +36,22 @@ void drivercontrol() {
   // Place drive control code here, inside the loop
     while (true) {
       if (Controller1.ButtonA.pressing()) { // If button A is pressed
-        //motor1.spin(vex::directionType::fwd, 50, vex::velocityUnits::pct); // Set motor1 to spin forward at 100% velocity
-        motor2.spin(vex::directionType::fwd, 50, vex::velocityUnits::pct); // Set motor2 to spin forward at 100% velocity
+        //motor11.spin(vex::directionType::fwd, 50, vex::velocityUnits::pct); // Set motor11 to spin forward at 100% velocity
+        motor20.spin(vex::directionType::fwd, 50, vex::velocityUnits::pct); // Set motor20 to spin forward at 100% velocity
       } else if (Controller1.ButtonB.pressing()) { // If button B is pressed
-        //motor1.spin(vex::directionType::rev, 50, vex::velocityUnits::pct); // Set motor1 to spin in reverse at 100% velocity
-        motor2.spin(vex::directionType::rev, 50, vex::velocityUnits::pct); // Set motor2 to spin in reverse at 100% velocity
+        //motor11.spin(vex::directionType::rev, 50, vex::velocityUnits::pct); // Set motor11 to spin in reverse at 100% velocity
+        motor20.spin(vex::directionType::rev, 50, vex::velocityUnits::pct); // Set motor20 to spin in reverse at 100% velocity
       } else { // If the buttons are not pressed
-        //motor1.stop(); // Stop motor1
-        motor2.stop(); // Stop motor2
+        //motor11.stop(); // Stop motor11
+        motor20.stop(); // Stop motor20
       }
       // If the limit switch is pressed (returns true), print "Limit switch pressed"
       if (LimitSwitch.pressing()) {
         Brain.Screen.print("Limit switch pressed");
         Brain.Screen.newLine();
         // If the limit switch is pressed, stop the motors
-        //motor1.stop();
-        motor2.stop();
+        //motor11.stop();
+        motor20.stop();
         // Wait 2 seconds
         vex::task::sleep(2000);        
       }
